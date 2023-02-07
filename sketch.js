c6a962a59f1a95cd4565f407124b55de353f4b0f
alxndrpsclt <alxndr.psclt@gmail.com>
2023-02-07 03:32:17 +0100

    Start of the p5.js script, based on the example in the documentation

--- sketch.js
+++ sketch.js
@@ -1,283 +1,37 @@
+let osc, playing, freq, amp;
+
 function setup() {
-
-  // Library setup
-  createCanvas( window.screen.width * window.devicePixelRatio, window.screen.height* window.devicePixelRatio);
-  //noCursor();
-  frameRate(5);
-  colorMode(HSB);
-
-  // Global image setup
-  ROTATE_CONFIG = false;
-  CONFIG_CHANGE_RANDOM_PERIOD = 200;
-  ELEMENT_PERTUBATEUR = false;
-  PERTURBATION_DENSITY = 0.02;
-  PERTURBATION_PERIOD= 20;
-
-  SQUARE_SIZE = 30;
-
-
-  // Game of life display setup
-  //numrows = 20;
-  //numcols = 32;
-
-  numrows = window.screen.height * window.devicePixelRatio / SQUARE_SIZE;
-  numcols = window.screen.width * window.devicePixelRatio / SQUARE_SIZE;
-  console.log(numrows);
-  console.log(numcols);
-
-
-  // squareres = 12; // Used for classic matrix display; currently not maintained
-  //radius = 82;
-  radius = max(window.screen.width* window.devicePixelRatio, window.screen.height * window.devicePixelRatio) / numrows;
-  CENTER_X = window.screen.width * window.devicePixelRatio / 2;
-  CENTER_Y = window.screen.height * window.devicePixelRatio / 2;
-  // Default config; can be overrided later by a named, or custom config
-  startDensity = 0.0005;
-  BACKGROUND_FADE = '1';
-  refreshColor = `rgba(0,0,0,${BACKGROUND_FADE})`;
-  RANDOM_CENTER_DISPLACEMENT_X=20;
-  RANDOM_CENTER_DISPLACEMENT_Y=20;
-
-  background(refreshColor);
-
-  // CONFIGS
-
-  // Classic configs
-  Classic = { startDensity: 0.2, genesisRule: [3], survivalRule: [2,3] };
-  DiscoCrystal = { startDensity : 0.002, genesisRule: [1], survivalRule: [1] };
-
-  // Experimental configs
-  MasseImpact5 = { genesisRule: [0,4,5], survivalRule: [0,1,2,3] };
-  MasseImpact1 = { genesisRule: [0,4,1], survivalRule: [0,1,2,3] };
-  MasseImpact2 = { genesisRule: [0,4,2], survivalRule: [0,1,2,3] };
-
-  Assimilation = { startDensity: 0.02, genesisRule: [2,5], survivalRule: [2,3,6] };
-  Dissimilation = { startDensity: 0.000000000002, genesisRule: [1], survivalRule: [2,3,4] };
-
-  Reject = { startDensity: 0.18, genesisRule: [3,4,5], survivalRule: [4,5,6,7] };
-  Experiment626 = { startDensity : 0.008, genesisRule: [1,4], survivalRule: [2] };
-
-  configs = { DiscoCrystal, Classic, Dissimilation };
-  currentConfig = "DiscoCrystal";
-  currentConfig = "Reject";
-  currentConfig = "Classic";
-
-  loadConfig(currentConfig);
-
-  // Initialise to random matrix
-  currentMatrix = [];
-  for (let i = 0, len = numrows; i < len; i++) {
-    currentMatrix.push([]);
-    for (let j = 0, len2 = numcols; j < len2; j++) {
-      if (random(1)<startDensity) {
-        currentMatrix[i].push(1);
-      } else {
-        currentMatrix[i].push(0);
-      }
-    }
-  };
-
-  nextMatrix = [];
-  for (let i = 0, len = numrows; i < len; i++) {
-    nextMatrix.push([]);
-    for (let j = 0, len2 = numcols; j < len2; j++) {
-      nextMatrix[i].push(0);
-    }
-  };
-
-  prevMatrix = [];
-  for (let i = 0, len = numrows; i < len; i++) {
-    prevMatrix.push([]);
-    for (let j = 0, len2 = numcols; j < len2; j++) {
-      prevMatrix[i].push(0);
-    }
-  };
-
-  ageMatrix = [];
-  for (let i = 0, len = numrows; i < len; i++) {
-    ageMatrix.push([]);
-    for (let j = 0, len2 = numcols; j < len2; j++) {
-      ageMatrix[i].push(0);
-    }
-  };
-
-  drawMatrix = [];
-  for (let i = 0, len = numrows; i < len; i++) {
-    drawMatrix.push([]);
-    for (let j = 0, len2 = numcols; j < len2; j++) {
-      drawMatrix[i].push(0);
-    }
-  };
-
-}
-
-function loadConfig(configName) {
-  if (configName in configs) {
-    Object.keys(configs[configName]).forEach(key => {
-      window[key] = configs[configName][key];
-      console.log(`Loading config ${key}.`);
-    })
-  }
-}
-
-function countNeighbours(matrix, x, y) {
-  return (matrix[((x -1 + numrows) % numrows)][((y - 1 + numcols) % numcols)] + matrix[((x -1 +numrows) % numrows)][y]+ matrix[((x -1 +numrows) % numrows)][((y + 1) % numcols)]+ matrix[x][((y - 1 + numcols) % numcols)]+ matrix[x][((y + 1) % numcols)]+ matrix[((x +1) % numrows)][((y - 1 +numcols) % numcols)]+ matrix[((x +1) % numrows)][y]+ matrix[((x +1) % numrows)][((y + 1) % numcols)])
-}
-
-function checkRules(neighboursCount, rules) {
-  trial = false;
-  for (let k = 0, len = rules.length; k < len; k++) {
-    trial = trial || (neighboursCount == rules[k]);
-  }
-  return trial;
-}
-
-var randomPropertyName = function (obj) {
-  var keys = Object.keys(obj);
-  return keys[ keys.length * Math.random() << 0];
-};
-
-function mousePressed() {
-  console.log("clicked");
-  clickedCellX = int(mouseX / SQUARE_SIZE);
-  clickedCellY = int(mouseY / SQUARE_SIZE);
-  //prevMatrix[clickedCellY][clickedCellX]=1;
-  drawMatrix[clickedCellY][clickedCellX]=1;
-  //nextMatrix[clickedCellY][clickedCellX]=1;
-  return false;
-}
-
-touchStarted = mousePressed;
-
-function mouseDragged() {
-  console.log("Dragged");
-  clickedCellX = int(mouseX / SQUARE_SIZE);
-  clickedCellY = int(mouseY / SQUARE_SIZE);
-  //prevMatrix[clickedCellY][clickedCellX]=1;
-  drawMatrix[clickedCellY][clickedCellX]=1;
-  //nextMatrix[clickedCellY][clickedCellX]=1;
-  return false;
-}
-
-function mouseReleased() {
-  console.log("Mouse released");
-  for (let i = 0, len = drawMatrix.length; i < len; i++) {
-    for (let j = 0, len2 = drawMatrix[i].length; j < len2; j++) {
-      if (drawMatrix[i][j] == 1) {
-        currentMatrix[i][j]=1;
-        drawMatrix[i][j]=0;
-      }
-    }
-  }
-}
-
-
-function touchStarted() {
-  clickedCellX = int(mouseX / SQUARE_SIZE);
-  clickedCellY = int(mouseY / SQUARE_SIZE);
-  //prevMatrix[clickedCellY][clickedCellX]=1;
-  drawMatrix[clickedCellY][clickedCellX]=1;
-  //nextMatrix[clickedCellY][clickedCellX]=1;
-  stroke(150, cellSaturation, cellBrightness, cellAlpha);
-  fill(150, cellSaturation, cellBrightness, cellAlpha);
-  square(10*(SQUARE_SIZE), 10*SQUARE_SIZE, SQUARE_SIZE, 10, 10, 10);
-  return false;
-}
-
-function touchMoved() {
-  clickedCellX = int(mouseX / SQUARE_SIZE);
-  clickedCellY = int(mouseY / SQUARE_SIZE);
-  //prevMatrix[clickedCellY][clickedCellX]=1;
-  drawMatrix[clickedCellY][clickedCellX]=1;
-  //nextMatrix[clickedCellY][clickedCellX]=1;
-  stroke(150, cellSaturation, cellBrightness, cellAlpha);
-  fill(150, cellSaturation, cellBrightness, cellAlpha);
-  square(10*(SQUARE_SIZE), 10*SQUARE_SIZE, SQUARE_SIZE, 10, 10, 10);
-  return false;
-}
-function touchEnded() {
-  clickedCellX = int(mouseX / SQUARE_SIZE);
-  clickedCellY = int(mouseY / SQUARE_SIZE);
-  //prevMatrix[clickedCellY][clickedCellX]=1;
-  drawMatrix[clickedCellY][clickedCellX]=1;
-  //nextMatrix[clickedCellY][clickedCellX]=1;
-  stroke(150, cellSaturation, cellBrightness, cellAlpha);
-  fill(150, cellSaturation, cellBrightness, cellAlpha);
-  square(10*(SQUARE_SIZE), 10*SQUARE_SIZE, SQUARE_SIZE, 10, 10, 10);
-  return false;
+  let cnv = createCanvas(100, 100);
+  cnv.mousePressed(playOscillator);
+  osc = new p5.Oscillator('sine');
 }
 
 function draw() {
+  background(220)
+  freq = constrain(map(mouseX, 0, width, 100, 500), 100, 500);
+  amp = constrain(map(mouseY, height, 0, 0, 1), 0, 1);
 
-  //clear();
-  background(refreshColor);
-  //noFill();
+  text('tap to play', 20, 20);
+  text('freq: ' + freq, 20, 40);
+  text('amp: ' + amp, 20, 60);
 
-  for (let i = 0, len = currentMatrix.length; i < len; i++) {
-    for (let j = 0, len2 = currentMatrix[i].length; j < len2; j++) {
-
-      neighboursCount = countNeighbours(currentMatrix, i,j);
-
-      // Checking the Rules of life to create the nextMatrix
-      nextMatrix[i][j] = 0;
-
-
-      if (currentMatrix[i][j] == 0) {
-        if (checkRules(neighboursCount, genesisRule)) {
-          nextMatrix[i][j] = 1;
-        };
-      };
-      if (currentMatrix[i][j] == 1) {
-        if (checkRules(neighboursCount, survivalRule)) {
-          nextMatrix[i][j] = 1;
-        };
-      };
-
-      //stroke(230 + random(25));
-
-      if (ageMatrix[i][j] > 30) {
-        cellHue = int(random(10, 50));
-      } else {
-        cellHue = int(random(220, 240));
-      }
-      cellSaturation = int(random(100, 140));
-      cellBrightness = int(random(130, 190));
-      cellAlpha = random(0.4,0.9);
-
-      // s+5)trokeColor = `rgba(${cellHue},${cellSaturation},${cellBrightness},${BACKGROUND_FADE})`;
-      // stroke(strokeColor);
-      if (currentMatrix[i][j] == 1) {
-        stroke(cellHue, cellSaturation, cellBrightness, cellAlpha);
-        fill(cellHue, cellSaturation, cellBrightness, cellAlpha);
-        ageMatrix[i][j]+=1;
-      } else {
-        ageMatrix[i][j]=0;
-      }
-      //
-      //strokeWeight(20/sqrt(i+1));
-      strokeWeight(random(8));
-
-      if (drawMatrix[i][j] == 1) {
-        stroke(150, cellSaturation, cellBrightness, cellAlpha);
-        fill(150, cellSaturation, cellBrightness, cellAlpha);
-        square(j*(SQUARE_SIZE), i*SQUARE_SIZE, SQUARE_SIZE, 10, 10, 10);
-      }
-
-      if (currentMatrix[i][j] == 1) {
-        //arc(CENTER_X+centers_rotations[i].x, CENTER_Y+centers_rotations[i].y, radius*(i+1) + random_radiuses[i], radius*(i+1) + random_radiuses[i], (j+0.1)*2*PI/numcols + circles_rotations[i].value, (j+0.9)*2*PI/numcols + circles_rotations[i].value);
-
-        square(j*(SQUARE_SIZE), i*SQUARE_SIZE, SQUARE_SIZE,10);
-
-        //arc(CENTER_X+centers_rotations[i].x, CENTER_Y+centers_rotations[i].y, radius*(i+1) + random_radiuses[i], radius*(i+1) + random_radiuses[i], (j+0.1)*2*PI/numcols + circles_rotations[i].value, (j+0.9)*2*PI/numcols + circles_rotations[i].value);
-      }
-    }
+  if (playing) {
+    // smooth the transitions by 0.1 seconds
+    osc.freq(freq, 0.1);
+    osc.amp(amp, 0.1);
   }
+}
 
-  for (let i = 0, len = numrows; i < len; i++) {
-    arrayCopy(currentMatrix[i], prevMatrix[i]);
-    arrayCopy(nextMatrix[i], currentMatrix[i]);
-  }
+function playOscillator() {
+  // starting an oscillator on a user gesture will enable audio
+  // in browsers that have a strict autoplay policy.
+  // See also: userStartAudio();
+  osc.start();
+  playing = true;
+}
 
-
-
+function mouseReleased() {
+  // ramp amplitude to 0 over 0.5 seconds
+  osc.amp(0, 0.5);
+  playing = false;
 }
